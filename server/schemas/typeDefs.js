8308b67c0f8f5421f079e73c6ed44c238e26eb8c
McKenna <Mckennaburch19@gmail.com>
2022-05-23 20:49:40 -0500

    added typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,57 @@
+const typeDefs = gql`
+  type Category {
+    _id: ID
+    name: String
+  }
+
+  type Class {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    quantity: Int
+    category: Category
+  }
+
+  type Payment {
+    _id: ID
+    purchaseDate: String
+    Class: [Class]
+  }
+
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    Payment: [Payment]
+  }
+
+  type Checkout {
+    session: ID
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    categories: [Category]
+    Class(category: ID, name: String): [Class]
+    Class(_id: ID!): Class
+    user: User
+    payment(_id: ID!): Payment
+    checkout(Class: [ID]!): Checkout
+  }
+
+  type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addPayment(Class: [ID]!): Payment
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    updateClass(_id: ID!, quantity: Int!): Class
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
