388cf97f00a56740aeed2f9438aa94108142cc66
Alec Harvey <ruinhard@gmail.com>
2022-05-25 13:35:21 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,6 @@
 const express = require('express');
-<<<<<<< HEAD
-=======
 const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
->>>>>>> feature/dashboard
 const path = require('path');
 const routes = require('./routes');
 const {ApolloServer}=require("apollo-server-express")
@@ -26,23 +23,12 @@ startServer();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-<<<<<<< HEAD
 // if we're in production, serve client/build as static assets
-=======
->>>>>>> feature/dashboard
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-<<<<<<< HEAD
 app.use(routes);
-=======
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-//app.use(routes);
->>>>>>> feature/dashboard
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
