3e5229a363d994da2d54c0e1e0de2c32642b3e7b
Alex Contreras <alex.code29@gmail.com>
2022-05-25 13:04:24 -0500

    conneted the stylesheets to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {ApolloClient,InMemoryCache,ApolloProvider}from "@apollo/client"
-import {BrowserRouter as Router, Route} from "react-router-dom"
-const client= new ApolloClient({
-  uri:"/graphql",
-=======
 import React, { useState } from 'react'
 import Dashboard from './clientpages/dashboard'
+import './index.css'
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 const client = new ApolloClient({
   uri: "/graphql",
->>>>>>> feature/dashboard
   cache: new InMemoryCache()
 })
 function App() {
@@ -34,10 +25,6 @@ function App() {
   const [currentOption, setCurrentOption] = useState(options[0])
 
   return (
-<<<<<<< HEAD
-=======
-
->>>>>>> feature/dashboard
     <ApolloProvider client={client}>
       <Router>
         <div className='main-div'>
