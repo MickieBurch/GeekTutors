679f4f504e37e4589c9e20e0b64752da5faca8b3
Chris Snyder <csnyder332@gmail.com>
2022-05-27 11:41:21 -0500

    calendar update

--- client/src/clientpages/calendar.js
+++ client/src/clientpages/calendar.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
-import Calendar from 'react-calendar';
+import { Calendar } from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 function GeekCalendar() {
   const [value, onChange] = useState(new Date());
 
+
   return (
     <div>
       <Calendar onChange={onChange} value={value} />
+      {console.log(value)}
     </div>
   );
 }
