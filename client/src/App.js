8274715d023045420087111461ff8c97fb0638e8
Alex Contreras <alex.code29@gmail.com>
2022-05-28 13:45:09 -0500

    changed file paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 import Header from './clientpages/header'
 import Home from './clientpages/dashboard'
-import Login from './clientpages/LoginForm'
-import Signup from './clientpages/SignupForm'
 import Tutor from './clientpages/tutor'
 import { Container } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,6 +10,7 @@ import { BrowserRouter as Router } from "react-router-dom"
 import 'react-calendar/dist/Calendar.css';
 import LoginForm from './clientpages/LoginForm'
 import SignupForm from './clientpages/SignupForm'
+
 const client = new ApolloClient({
   uri: "/graphql",
   cache: new InMemoryCache()
--- client/src/clientpages/LoginForm.js
+++ client/src/clientpages/LoginForm.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+import { loginUser } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
