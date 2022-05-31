4127d03ec8a694b54c5f572ca973a60666574858
Alex Contreras <alex.code29@gmail.com>
2022-05-30 19:40:33 -0500

    made cards into a componet and query get all users

---
+++ client/src/componets/Card/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Col, Card } from 'react-bootstrap'
+import Ronald from '../../assets/img/Ronald.jpg'
+import Auth from '../../utils/auth'
+
+import { useQuery } from '@apollo/client';
+import { GET_TUTORS } from '../../utils/queries'
+
+
+function Cards({setCurrentTab}) {
+
+    const { data: tutors } = useQuery(GET_TUTORS)
+    console.log(tutors)
+  
+
+    function handleSaveTutor() {
+        //mutation to add tutor to user
+       setCurrentTab("tutor")
+    }
+    return (
+        <Col>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant='top' src={Ronald} />
+                <Card.Body>
+                    <Card.Title>
+                        Alex Contreras
+                    </Card.Title>
+                    <Card.Text>Subjects Offered: </Card.Text>
+                </Card.Body>
+                <div className="list-group">
+                    <li className="list-group-item list-group-item">HTML</li>
+                    <li className="list-group-item list-group-item-disabled">CSS</li>
+                    <li className="list-group-item list-group-item-disabled">Javascript</li>
+                </div>
+                     {Auth.loggedIn() ? <button onClick={(e) => { e.preventDefault(); handleSaveTutor() }}>Save</button> : null}
+            </Card>
+        </Col>
+    )
+};
+
+
+export default Cards
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -10,6 +10,18 @@ export const QUERY_ME = gql`
 }
 `;
 
-export const GET_ARTICLES_BY_PROCTOR_ID=gql`
+//export const GET_ARTICLES_BY_PROCTOR_ID=gql`
   
+//`
+
+export const GET_TUTORS = gql`
+  query GetAllUsers($token: String){
+    GetAllUsers(token: $token) {
+      _id 
+      firstName
+      lastName
+      email
+      isTutor
+    }
+  }
 `
