6a31edbd8d371279b37c19226b5fd4e7456ca3ce
Alec Harvey <ruinhard@gmail.com>
2022-05-31 21:09:14 -0500

    changed collection name to geek tutors

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,4 @@
 const mongoose= require("mongoose")
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mernshopping')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/geektutor')
 module.exports=mongoose.connection
